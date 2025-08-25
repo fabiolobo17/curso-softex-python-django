4d2ffa9df955bfc75bf06abd971570c2e67b8dbe
kauazin21 <26785511+fabiolobo17@users.noreply.github.com>
2025-08-25 15:52:04 -0300

    jogo de advinha

---
+++ aula7/ex2.py
@@ -0,0 +1,9 @@
+numero = 7
+contador = 0
+for contador in range(1,6):
+    ten = int(input("Digite um número: "))
+    if ten == numero:
+        print("Você acertou!")
+        break
+    elif:
+        print("Você errou!")
