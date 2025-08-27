055667395a79c7b1b1e784f6248abe21259f89ed
kauazin21 <26785511+fabiolobo17@users.noreply.github.com>
2025-08-27 15:38:02 -0300

    numero de telefone

---
+++ aula 8/desafio.py
@@ -0,0 +1,3 @@
+lado1 = int(input("Digite o primeiro lado")
+lado2 = int(input("Digite o segundo lado")
+lado3 = int(input("Digite o terceiro lado")
---
+++ aula 8/ex1.py
@@ -0,0 +1,5 @@
+senha = "123456"
+dig = print("Digite sua senha: ")
+while dig == senha:
+    print(Senha correta)
+    
