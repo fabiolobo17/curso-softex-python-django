2cf1c8ff8a12470e410694535e6ed5e35fa4df8b
fabiolobo17 <26785511+fabiolobo17@users.noreply.github.com>
2025-09-03 14:54:31 -0300

    alteração de pasta

---
+++ modulo 1/Aula 9/ex1.py
@@ -0,0 +1,2 @@
+numeros = [1, 5, 2, 8, 5, 3, 5]
+print(f"O numero cinco aparece {numeros.count(5)} vezes")
---
+++ modulo 1/Aula 9/ex3.py
@@ -0,0 +1,11 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+primos = []
+
+for numero in numeros:
+    eh_primo = True
+    if numero < 2:
+        eh_primo = False
+    else:
+        for i in range(2, numero):
+            if numero % 
+    
