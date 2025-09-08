549a87eae851dc7d2c0ddddb18b05139dfb6aff3
Fabio Lobo Assumpção <26785511+fabiolobo17@users.noreply.github.com>
2025-09-08 09:41:25 -0300

    atuallizado desafio

--- modulo 2/Aula 2  - Listas, Tuplas e Conjuntos/Listas/desafio.py
+++ modulo 2/Aula 2  - Listas, Tuplas e Conjuntos/Listas/desafio.py
@@ -1,5 +1,5 @@
 registros_acessos = []
-usuarios_sucesso = set()
+usuarios_ok = set()
 tempo_total = 0
 
 while True:
@@ -20,15 +20,9 @@ while True:
     # Laço para garantir que o status seja válido
     while True:
         try:
-<<<<<<< HEAD
             status = int(input("""\nDigite:
             1 - sucesso 
             2 - falha\n"""))
-=======
-            status = int(input("Digite:\n
-            1 - sucesso 
-            \n2 - falha"))
->>>>>>> b9a113035688fe18328ab9e22f903bca2329d11e
         except ValueError:
             print("Erro: digite apenas 1 ou 2.\n")
             continue
@@ -50,19 +44,12 @@ while True:
     # Definindo status em texto e atualizando dados
     status_texto = "sucesso" if status == 1 else "falha"
     if status == 1:
-        usuarios_sucesso.add(usuario)
+        usuarios_ok.add(usuario)
         tempo_total += duracao
 
     # Adiciona o registro à lista
     registros_acessos.append((usuario, status_texto, duracao))
 
-<<<<<<< HEAD
 print("\n Registro de acessos:", registros_acessos)
-print("Usuários com acesso bem-sucedido:", usuarios_sucesso)
+print("Usuários com acesso bem-sucedido:", usuarios_ok)
 print("Tempo total de sessões bem-sucedidas:", tempo_total, "minutos")
-=======
-# 
-print("\n Registro de acessos:", registros_acessos)
-print("Usuários com acesso bem-sucedido:", usuarios_sucesso)
-print("Tempo total de sessões bem-sucedidas:", tempo_total, "minutos")
->>>>>>> b9a113035688fe18328ab9e22f903bca2329d11e
