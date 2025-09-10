00c530217ef908b198b2cb940458be3b6c241231
Fabio Lobo Assumpção <26785511+fabiolobo17@users.noreply.github.com>
2025-09-10 11:28:58 -0300

    feat: update de dicionario - aula 03

---
+++ modulo 2/Aula 3 - Dicionários/explicacao2.py
@@ -0,0 +1,14 @@
+produto_info = {"nome": "Smartphone", "cor": "Preto"}
+
+detalhes_estoque = {"preco": 1500.00, "estoque": 50}
+
+# Usando .update() para adicionar os detalhes de estoque
+# produto_info |= detalhes_estoque
+#produto_info.update(detalhes_estoque)
+
+
+# união de dois dicionários fazendo update de chaves e valores
+novo_produto = produto_info | detalhes_estoque
+
+print(novo_produto)
+# Saída: {'nome': 'Smartphone', 'cor': 'Preto', 'preco': 1500.0, 'estoque': 50}
