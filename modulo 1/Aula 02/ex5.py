89f7441c7570b8d2e31e62c5599e016c0777e946
Fabio Lobo Assumpção <26785511+fabiolobo17@users.noreply.github.com>
2025-09-11 02:42:03 -0300

    feat: verifica qual numero é maior

---
+++ modulo 1/Aula 02/ex5.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+if num1 == num2:
+    print("Os numeros são iguais.")
+else:
+    if num1 > num2:
+        print("O primeiro é maior que o segundo.")
+    else:
+        print("O segundo é maior que o primeiro.")
