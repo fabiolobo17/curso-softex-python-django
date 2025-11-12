a1e091067ee33bf3290165299df264eecde4ca25
fabiolobo17 <26785511+fabiolobo17@users.noreply.github.com>
2025-11-12 15:50:17 -0300

    feat: incluindo um form

--- Modulo_4/core/views.py
+++ Modulo_4/core/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
-from .models import Tarefa
+from django.shortcuts import render, redirect  # 1. Importe o 'redirect' 
+from .models import Tarefa 
+from .forms import TarefaForm                 
 def home(request): 
-    # 2. Use o ORM para buscar os dados! 
-# Tarefa.objects.all() significa: "Pegue todas as linhas da tabela Tarefa" 
-    todas_as_tarefas = Tarefa.objects.all() 
-# 1. Crie seu dicionário de contexto 
+# 2. Importe nosso novo 'TarefaForm' 
+# 3. Lógica de POST: Se o formulário foi enviado 
+    if request.method == 'POST': 
+# Cria uma instância do form e preenche com os dados do POST 
+        form = TarefaForm(request.POST) 
+         
+        # 4. O Django valida os dados (max_length, etc.) 
+        if form.is_valid(): 
+            # 5. Salva o objeto no banco de dados! 
+            form.save() 
+             
+            # 6. Redireciona de volta para a 'home' 
+            # Isso é o Padrão "Post-Redirect-Get" (PRG) 
+            return redirect('home') 
+         
+        # Se o form NÃO for válido, o código continua e  
+        # o 'form' (com os erros) será enviado para o template 
+     
+    # 7. Lógica de GET: Se o usuário apenas visitou a página 
+    else: 
+        form = TarefaForm()  # Cria um formulário vazio 
+ 
+    # 8. A busca de dados (fora dos 'ifs', pois é necessária sempre) 
+    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em') # Ordena pelas mais novas 
+ 
+    # 9. Atualize o contexto para incluir o formulário 
     context = { 
         'nome_usuario': 'Fabio', 
-        'tecnologias': ['Python', 'Django', 'Models', 'Admin'],
-        'tarefas': todas_as_tarefas  # 4. Adicione as tarefas ao contexto 
+        'tecnologias': ['Python', 'Django', 'Models', 'Forms'], 
+        'tarefas': todas_as_tarefas,   # 10. Envie o 'form' (vazio ou com erros) para o template 
     } 
-# 2. Passe o 'context' como terceiro argumento do render() 
+     
     return render(request, 'home.html', context) 
